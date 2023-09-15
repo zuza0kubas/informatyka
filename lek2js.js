5102b2842b03e0d93d0bb452607f8a730f8cfb5d
zuza0kubas <113593908+zuza0kubas@users.noreply.github.com>
2023-09-15 14:09:42 +0200

    Add files via upload

---
+++ lek2js.js
@@ -0,0 +1,39 @@
+var box = document.getElementById('3');
+
+function sh(x){
+    box.value += x;
+    
+    if (x=='c'){
+        box.value = '';
+}
+
+function wynik() {
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+    if ()
+}
+function dodawanie() {
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+    if ()
+}
+function odejmowanie() {
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+    if ()
+}
+function mnozenie() {
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+    if ()
+}
+function dzielenie() {
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+    if ()
+}
